7ca067327a9c7c6d5f7826ca83877e34e70d9a11
Pham-Thanh-Huy <huypham3062k3@gmail.com>
2024-12-19 23:24:26 +0700

    update template

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Header from './layout/header-footer/Header'
-import Footer from "./layout/header-footer/Footer.tsx";
-import Home from './layout/body/home/Home.tsx';
+import { BrowserRouter, Routes } from 'react-router-dom'
+
 
 function App() {
 
   return (
     <div>
       <BrowserRouter>
-        <Header />
+
         <Routes>
-          <Route path='/' element={<Home />} />
+          {/* <Route path='/' element={<Home />} /> */}
         </Routes>
-        <Footer />
+
       </BrowserRouter>
     </div>
   )
